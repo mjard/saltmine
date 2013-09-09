0c9087cdd10a8fa747e4cf790cb9f12a05285c62
Michael Jard <francisjard@gmail.com>
2013-09-08 22:50:12 -0700

    :(

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ var store = sessions.NewCookieStore([]byte("something-not-very-secret"))
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func betHandler(w http.ResponseWriter, r *http.Request, s *sessions.Session) {
-    user := s.Values["username"]
-    choice := r.FormValue("choice")
-    amount := r.FormValue("amount")
+	user := s.Values["username"]
+	choice := r.FormValue("choice")
+	amount := r.FormValue("amount")
 
-    log.Printf("%v bet %v on %v", user, amount, choice)
+	log.Printf("%v bet %v on %v", user, amount, choice)
 
 	http.Redirect(w, r, "/", http.StatusFound)
 }
@@ -25,15 +25,15 @@ func betHandler(w http.ResponseWriter, r *http.Request, s *sessions.Session) {
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := store.Get(r, "login")
 	if err == nil {
-	    session.Values["username"] = r.FormValue("username")
-	    session.Save(r, w)
-    }
+		session.Values["username"] = r.FormValue("username")
+		session.Save(r, w)
+	}
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request, s *sessions.Session) {
-    s.Options.MaxAge = -1
-    s.Save(r, w)
+	s.Options.MaxAge = -1
+	s.Save(r, w)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
@@ -44,8 +44,8 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	view := templates.Lookup("main.html")
 	if session.IsNew {
-	    view = templates.Lookup("login.html")
-	} 
+		view = templates.Lookup("login.html")
+	}
 	view.Execute(w, nil)
 }
 
@@ -54,6 +54,6 @@ func main() {
 	http.HandleFunc("/login", loginHandler)
 	http.Handle("/logout", ValidateSession(logoutHandler, "/"))
 	http.Handle("/bet", ValidateSession(betHandler, "/"))
-    http.Handle("/static/", http.FileServer(http.Dir(".")))
+	http.Handle("/static/", http.FileServer(http.Dir(".")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- session.go
+++ session.go
@@ -1,36 +1,34 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/sessions"
+	"github.com/gorilla/sessions"
 )
 
-type SessionHandler func (http.ResponseWriter, *http.Request, *sessions.Session)
+type SessionHandler func(http.ResponseWriter, *http.Request, *sessions.Session)
 
 type SessionValidator struct {
-    handler SessionHandler
-    redirect string
+	handler  SessionHandler
+	redirect string
 }
 
 func ValidateSession(handler SessionHandler, path string) SessionValidator {
-    return SessionValidator{handler, path}
+	return SessionValidator{handler, path}
 }
 
 func (s SessionValidator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    session, err := store.Get(r, "login")
-    if err != nil {
-        log.Println("[session validator] error reading session")
-        http.Redirect(w, r, s.redirect, http.StatusFound)
-        return
-    }
+	session, err := store.Get(r, "login")
+	if err != nil {
+		log.Println("[session validator] error reading session")
+		http.Redirect(w, r, s.redirect, http.StatusFound)
+		return
+	}
 
-    if session.IsNew {
-        http.Redirect(w, r, s.redirect, http.StatusFound)
-        return
-    }
-    s.handler(w, r, session)
+	if session.IsNew {
+		http.Redirect(w, r, s.redirect, http.StatusFound)
+		return
+	}
+	s.handler(w, r, session)
 }
-
-
