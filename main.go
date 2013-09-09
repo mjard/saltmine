c6ba5bcb432897f58e8e87d0bb5174ab332c2894
Michael Jard <francisjard@gmail.com>
2013-09-08 22:41:52 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	//"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+)
+
+var store = sessions.NewCookieStore([]byte("something-not-very-secret"))
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func betHandler(w http.ResponseWriter, r *http.Request, s *sessions.Session) {
+    user := s.Values["username"]
+    choice := r.FormValue("choice")
+    amount := r.FormValue("amount")
+
+    log.Printf("%v bet %v on %v", user, amount, choice)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "login")
+	if err == nil {
+	    session.Values["username"] = r.FormValue("username")
+	    session.Save(r, w)
+    }
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request, s *sessions.Session) {
+    s.Options.MaxAge = -1
+    s.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "login")
+	if err != nil {
+		log.Println("error reading session")
+	}
+	view := templates.Lookup("main.html")
+	if session.IsNew {
+	    view = templates.Lookup("login.html")
+	} 
+	view.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.Handle("/logout", ValidateSession(logoutHandler, "/"))
+	http.Handle("/bet", ValidateSession(betHandler, "/"))
+    http.Handle("/static/", http.FileServer(http.Dir(".")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ session.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/sessions"
+)
+
+type SessionHandler func (http.ResponseWriter, *http.Request, *sessions.Session)
+
+type SessionValidator struct {
+    handler SessionHandler
+    redirect string
+}
+
+func ValidateSession(handler SessionHandler, path string) SessionValidator {
+    return SessionValidator{handler, path}
+}
+
+func (s SessionValidator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    session, err := store.Get(r, "login")
+    if err != nil {
+        log.Println("[session validator] error reading session")
+        http.Redirect(w, r, s.redirect, http.StatusFound)
+        return
+    }
+
+    if session.IsNew {
+        http.Redirect(w, r, s.redirect, http.StatusFound)
+        return
+    }
+    s.handler(w, r, session)
+}
+
+
